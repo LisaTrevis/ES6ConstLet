e4ce5056133f62d9f34a58787d0bbb552c9131cd
Lisa Trevis <l_trevis@hotmail.com>
2018-04-25 10:22:32 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// ----- Const, Let & Var ------
+
+// In ES2015:
+
+// var name = "Lisa";
+
+// var title = "Software Engineer";
+
+// var hourlyWage = 40;
+
+// in ES6:
+
+// const name = "Lisa";
+
+// let title = " Software Engineer";
+
+// // Sometime later:
+// title = "Senior Software Engineer";
+
+// let hourlyWage = 40;
+
+// // Sometime later:
+// hourlyWage = 60;
+
+
+
+
+
+
+
+
+
