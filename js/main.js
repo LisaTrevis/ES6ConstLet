e99c5caf44822655b177d390eae12ade04d5bd45
Lisa Trevis <l_trevis@hotmail.com>
2019-11-12 18:20:57 -0800

    added addl notes to constlet

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 // ----- Const, Let & Var ------
 
-// In ES2015:
+// Const & Let inform others if they can expect a variable in our code to remain the same or change over time
+
+// Let doesn't exibit hoisting like var does, so it defaults to block scoping.
+
+// Const variables are usually written in all caps,
+// i.e. const AGE = 27;
+
+// Var is no longer needed but exists for backwards compatability
+
+// Prior to ES6:
 
 // var name = "Lisa";
 
@@ -23,10 +32,47 @@
 // hourlyWage = 60;
 
 
+// function count(targetString) {
+// 	const characters = ['a', 'e', 'i', 'o', 'u'];
+// 	let number = 0;
+
+// 	for (var i = 0; i < targetString.length; i++) {
+// 		if (characters.includes(targetString[i])) {
+// 			number++;
+// 		}
+// 	}
+
+// 	return number;
+// }
+
+// console.log(count("lkjsdaoilknsdfklsoilweknl"))
 
 
+// Refactor using const/let instead of var:
+// var statuses = [ 
+//   { code: 'OK', response: 'Request successful' },
+//   { code: 'FAILED', response: 'There was an error with your request' },
+//   { code: 'PENDING', response: 'Your reqeust is still pending' }
+// ];
+// var message = '';
+// var currentCode = 'OK';
 
+// for (var i = 0; i < statuses.length; i++) {
+//   if (statuses[i].code === currentCode) {
+//     message = statuses[i].response;
+//   }
+// }
 
+// const statuses = [ 
+//   { code: 'OK', response: 'Request successful' },
+//   { code: 'FAILED', response: 'There was an error with your request' },
+//   { code: 'PENDING', response: 'Your reqeust is still pending' }
+// ];
+// let message = '';
+// let currentCode = 'OK';
 
-
-
+// for (var i = 0; i < statuses.length; i++) {
+//   if (statuses[i].code === currentCode) {
+//     message = statuses[i].response;
+//   }
+// }
